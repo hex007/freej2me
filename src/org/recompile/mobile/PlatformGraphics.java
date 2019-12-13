c72904e810039694c053f4c1ebf2319774961d86
recompileorg <drichardson@recompile.org>
2019-12-12 20:00:45 -0500

    Fixes #63

--- src/org/recompile/mobile/PlatformGraphics.java
+++ src/org/recompile/mobile/PlatformGraphics.java
@@ -84,7 +84,6 @@ public class PlatformGraphics extends javax.microedition.lcdui.Graphics implemen
 		BufferedImage sub = canvas.getSubimage(subx, suby, subw, subh);
 
 		gc.drawImage(sub, x, y, null);
-		//drawImage2(sub, x, y);
 	}
 
 	public void drawArc(int x, int y, int width, int height, int startAngle, int arcAngle)
@@ -133,7 +132,6 @@ public class PlatformGraphics extends javax.microedition.lcdui.Graphics implemen
 		try
 		{
 			gc.drawImage(image.platformImage.getCanvas(), x, y, null);
-			//drawImage2(image, x, y);
 		}
 		catch (Exception e)
 		{
@@ -141,41 +139,49 @@ public class PlatformGraphics extends javax.microedition.lcdui.Graphics implemen
 		}
 	}
 
-	public void drawImage2(Image image, int x, int y)
+	public void drawImage2(Image image, int x, int y) // Internal use method called by PlatformImage
 	{
-		drawImage2(image.platformImage.getCanvas(), x, y);
+		gc.drawImage(image.platformImage.getCanvas(), x, y, null);
 	}
-	public void drawImage2(BufferedImage image, int x, int y)
+	public void drawImage2(BufferedImage image, int x, int y) // Internal use method called by PlatformImage
 	{
-		// Internal use method called by PlatformImage, Nokia Methods, etc.
+		gc.drawImage(image, x, y, null);
+	}
+
+	public void drawImage2Test(BufferedImage image, int x, int y)
+	{
+		// This Fixes some transparency issues with some images drawn 
+		// with Nokia drawImage in a few games.  
+		// There's probably a deeper underlying issue.
+		int row=0;
+		int col=0; 
+		int imgWidth = image.getWidth();
+		int imgHeight = image.getHeight();
+		int cwidth = canvas.getWidth();
+		int cheight = canvas.getHeight();
+		int width = imgWidth;
+		int height = imgHeight;
+
 		try
 		{
-			// gc.drawImage(image, x, y, null);
-			
-			int width = image.getWidth();
-			int height = image.getHeight();
-		
-			// Fixes some transparency issues
-			int[] datargb = image.getRGB(0,0, width,height, null,0,width);
-			try
-			{ canvas.setRGB(x, y, width, height, datargb, 0, width); } catch(Exception e) { }
-
-			/*
-			// also works okay, but very slow
-			int i=offset;
-			for(int row=0; row<height; row++)
+			int[] datargb = image.getRGB(0,0, imgWidth,imgHeight, null,0,imgWidth);
+			if(x+width>=cwidth+1) { width -= ((x+width)-cwidth); }
+			if(y+height>=cheight+1) { height-=((y+height)-cheight); }
+			for(row=0; row<height; row++)
 			{
-				for(int col=0; col<width; col++)
+				for(col=0; col<width; col++)
 				{
-					canvas.setRGB(col+x, row+y, temp.getRGB(col, row));
-					i++;
+					int c = datargb[col+ row*imgWidth] & 0xFFFFFFFF;
+					if(c!=0xFF000000)
+					{
+						canvas.setRGB(col+x, row+y, c);
+					}
 				}
 			}
-			*/
 		}
 		catch (Exception e)
 		{
-			//System.out.println("drawImage C:"+e.getMessage());
+			System.out.println("drawImage C:"+e.getMessage());
 		}
 	}
 
@@ -203,7 +209,6 @@ public class PlatformGraphics extends javax.microedition.lcdui.Graphics implemen
 				x = AnchorX(x, subw, anchor);
 				y = AnchorY(y, subh, anchor);
 				gc.drawImage(sub, x, y, null);
-				//drawImage2(sub, x, y);
 			}
 			else
 			{
@@ -211,7 +216,6 @@ public class PlatformGraphics extends javax.microedition.lcdui.Graphics implemen
 				x = AnchorX(x, sub.width, anchor);
 				y = AnchorY(y, sub.height, anchor);
 				gc.drawImage(sub.getCanvas(), x, y, null);
-				//drawImage2(sub, x, y);
 			}
 		}
 		catch (Exception e)
@@ -395,6 +399,7 @@ public class PlatformGraphics extends javax.microedition.lcdui.Graphics implemen
 		x = AnchorX(x, image.getWidth(), anchor);
 		y = AnchorY(y, image.getHeight(), anchor);
 		drawImage2(image, x, y);
+		//drawImage2Test(image, x, y);
 	}
 
 	public void drawPixels(byte[] pixels, byte[] transparencyMask, int offset, int scanlength, int x, int y, int width, int height, int manipulation, int format)
@@ -424,7 +429,7 @@ public class PlatformGraphics extends javax.microedition.lcdui.Graphics implemen
 
 				temp = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 				temp.setRGB(0, 0, width, height, data, 0, scanlength);
-				drawImage2(manipulateImage(temp, manipulation), x, y);
+				gc.drawImage(manipulateImage(temp, manipulation), x, y, null);
 			break;
 
 			case 1: // TYPE_BYTE_1_GRAY // used by Monkiki's Castles
@@ -441,7 +446,7 @@ public class PlatformGraphics extends javax.microedition.lcdui.Graphics implemen
 				}
 				temp = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 				temp.setRGB(0, 0, width, height, data, 0, scanlength);
-				drawImage2(manipulateImage(temp, manipulation), x, y);
+				gc.drawImage(manipulateImage(temp, manipulation), x, y, null);
 			break;
 
 			default: System.out.println("drawPixels A : Format " + format + " Not Implemented");
@@ -454,7 +459,7 @@ public class PlatformGraphics extends javax.microedition.lcdui.Graphics implemen
 		BufferedImage temp = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 		temp.setRGB(0, 0, width, height, pixels, offset, scanlength);
 		BufferedImage temp2 = manipulateImage(temp, manipulation);
-		drawImage2(temp2, x, y);
+		gc.drawImage(temp2, x, y, null);
 	}
 
 	public void drawPixels(short[] pixels, boolean transparency, int offset, int scanlength, int x, int y, int width, int height, int manipulation, int format)
@@ -469,7 +474,7 @@ public class PlatformGraphics extends javax.microedition.lcdui.Graphics implemen
 
 		BufferedImage temp = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 		temp.setRGB(0, 0, width, height, data, offset, scanlength);
-		drawImage2(manipulateImage(temp, manipulation), x, y);
+		gc.drawImage(manipulateImage(temp, manipulation), x, y, null);
 	}
 
 	public void drawPolygon(int[] xPoints, int xOffset, int[] yPoints, int yOffset, int nPoints, int argbColor)
