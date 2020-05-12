a531c175a2f617de61f7e9ff921fba0a723d447e
LONELY-WOLF <zvenayte@pisem.net>
2020-05-12 15:41:46 +0300

    Nokia M3D(O) added debug prints for API calls

--- src/com/nokia/mid/m3d/M3D.java
+++ src/com/nokia/mid/m3d/M3D.java
@@ -113,6 +113,7 @@ public class M3D
 
 	public void loadIdentity()
 	{
+		//System.out.println("loadIdentity");
 		identity(matrix);
 	}
 
@@ -127,6 +128,8 @@ public class M3D
 
 		double n = nearclip/65536.0;
 		double f = farclip/65536.0;
+
+		//System.out.println("frustrumxi("+l+", "+r+", "+t+", "+b+", "+n+", "+f+");");
 		
 		near = -n;
 		far = -f;
@@ -135,9 +138,12 @@ public class M3D
 
 	public void scalexi(int X, int Y, int Z)
 	{
+		//System.out.println("scalexi("+X+", "+Y+", "+Z+");");
 		double x = (X/65536.0);
 		double y = (Y/65536.0);
 		double z = (Z/65536.0);
+		
+		//System.out.println("scalexi("+x+", "+y+", "+z+");");
 
 		temps[0]  = x; temps[1]  = 0; temps[2]  = 0; temps[3]  = 0;
 		temps[4]  = 0; temps[5]  = y; temps[6]  = 0; temps[7]  = 0;
@@ -150,9 +156,13 @@ public class M3D
 
 	public void translatexi(int X, int Y, int Z)
 	{
+		//System.out.println("translatexi("+X+", "+Y+", "+Z+");");
 		double x = (X/65536.0);
 		double y = (Y/65536.0);
 		double z = (Z/65536.0);
+		
+		//System.out.println("translatexi("+x+", "+y+", "+z+");");
+
 		tempt[0]  = 1; tempt[1]  = 0; tempt[2]  = 0; tempt[3]  = 0;
 		tempt[4]  = 0; tempt[5]  = 1; tempt[6]  = 0; tempt[7]  = 0;
 		tempt[8]  = 0; tempt[9]  = 0; tempt[10] = 1; tempt[11] = 0;
@@ -164,6 +174,7 @@ public class M3D
 	
 	public void rotatexi(int Angle, int X, int Y, int Z)
 	{
+		//System.out.println("rotatexi("+Angle+", "+X+", "+Y+", "+Z+");");
 		double a = (Angle/65536.0)*0.0174533;
 
 		// (X, Y, Z) define an axis for rotation
@@ -172,6 +183,9 @@ public class M3D
 		if(X != 0)
 		{
 			// rotate on x
+
+			//System.out.println("rotatexi( X: "+Angle/65536+");");
+
 			tempr[0]  =  1; tempr[1]  =  0;            tempr[2]  =  0;           tempr[3]  =  0;
 			tempr[4]  =  0; tempr[5]  =  Math.cos(a);  tempr[6]  =  Math.sin(a); tempr[7]  =  0;
 			tempr[8]  =  0; tempr[9]  = -Math.sin(a);  tempr[10] =  Math.cos(a); tempr[11] =  0;
@@ -181,6 +195,9 @@ public class M3D
 		if(Y != 0)
 		{
 			// rotate on y
+
+			//System.out.println("rotatexi( Y: "+Angle/65536+");");
+
 			tempr[0]  =  Math.cos(a); tempr[1]  =  0; tempr[2]  = -Math.sin(a); tempr[3]  =  0;
 			tempr[4]  =  0;           tempr[5]  =  1; tempr[6]  =  0;           tempr[7]  =  0;
 			tempr[8]  =  Math.sin(a); tempr[9]  =  0; tempr[10] =  Math.cos(a); tempr[11] =  0;
@@ -190,6 +207,9 @@ public class M3D
 		if(Z != 0)
 		{
 			// rotate on z
+
+			//System.out.println("rotatexi( Z: "+Angle/65536+");");
+
 			tempr[0]  =  Math.cos(a); tempr[1]  =  Math.sin(a); tempr[2]  =  0; tempr[3]  =  0;
 			tempr[4]  = -Math.sin(a); tempr[5]  =  Math.cos(a); tempr[6]  =  0; tempr[7]  =  0;
 			tempr[8]  =  0;           tempr[9]  =  0;           tempr[10] =  1; tempr[11] =  0;
@@ -203,11 +223,13 @@ public class M3D
 
 	public void pushMatrix() // game doesn't seem to push more than one thing at a time
 	{ 
+		//System.out.println("pushMatrix()");
 		clone(stack, matrix);
 	}
 
 	public void popMatrix()
 	{
+		//System.out.println("popMatrix()");
 		clone(matrix, stack);
 	}
 
@@ -223,6 +245,7 @@ public class M3D
 
 	public void vertexPointerub(int a, int b, byte[] vertices) 
 	{
+		//System.out.println("vertexPointerub("+a+", "+b+")");
 		int i=0;
 		for(i=0; i<vertices.length; i+=3)
 		{
@@ -352,6 +375,7 @@ public class M3D
 
 	public void drawElementsub(int A, int B, byte[] facelist)
 	{
+		//System.out.println("drawElementsub("+A+", "+B+")");
 		byte a, b;
 		double x1, y1, z1, x2, y2, z2, x3, y3, z3, w1;
 		double u1,v1, u2,v2, u3,v3;
@@ -466,6 +490,7 @@ public class M3D
 		// The expected result.  No idea how to get there from here:
 		//vertexPointerub(3, 0, [ -100, 20, -100, 0, -20, 0, 100, 20, -100, ]); // 3, 0, len 9
 		//drawArrays(4, 0, 3);
+		//System.out.println("drawArrays("+a+", "+b+", "+c+")");
 		gc.setColor(color);
 		gc.fillRect(0,20, width, height);
 
@@ -592,6 +617,7 @@ public class M3D
 
 	private void applyMatrix(double[] m)
 	{
+		//System.out.println("applyMatrix()");
 		double x, y, z, w;
 		for(int i=0; i<vertCount; i+=3)
 		{	
