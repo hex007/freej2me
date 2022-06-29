5dda662760489ecbec5461f5d33821ddf8898b3b
AShiningRay <genesis45.ppc@gmail.com>
2022-06-29 17:51:45 -0300

    Revert "freej2me-lr: Fix rotated pointer coordinates."
    This reverts commit 35cb096b2c95b783347aac0e71824256b503750a.

--- src/org/recompile/freej2me/Libretro.java
+++ src/org/recompile/freej2me/Libretro.java
@@ -185,7 +185,7 @@ public class Libretro
 									}
 									else
 									{
-										Mobile.getPlatform().pointerReleased(-mousey+lcdWidth, mousex);
+										Mobile.getPlatform().pointerReleased(mousey, mousex);
 									}
 								break;
 
@@ -198,7 +198,7 @@ public class Libretro
 									}
 									else
 									{
-										Mobile.getPlatform().pointerPressed(-mousey+lcdWidth, mousex);
+										Mobile.getPlatform().pointerPressed(mousey, mousex);
 									}
 								break;
 
@@ -211,7 +211,7 @@ public class Libretro
 									}
 									else
 									{
-										Mobile.getPlatform().pointerDragged(-mousey+lcdWidth, mousex);
+										Mobile.getPlatform().pointerDragged(mousey, mousex);
 									}
 								break;
 
